8e7e24c88b2a578a36337f1e56cf4ac64ba377f8
Subramanya K S <subramanyaks2205@gmail.com>
2022-12-03 21:42:58 +0530

    added sign up

---
+++ src/components/SignUp.vue
@@ -0,0 +1,42 @@
+<template class="main">
+<img class="logo" alt="Vue logo" src="../assets/logo1.jpg">
+    <h1>SignUp</h1>
+    <div class="register">
+        <input type="text" placeholder="Enter the Name:" />
+        <input type="text" placeholder="Enter the Email:" />
+        <input type="password" placeholder="Enter the Password:" />
+        <button class="btn">Sign Up</button>
+    </div>
+</template>
+<script>
+export default {
+    name:'SignUp'
+}
+</script>
+<style >
+.logo{
+  width:100px;
+}
+.register input{
+    width:300px;
+    height:40px;
+    display:block;
+    padding-left: 20px;
+    margin-right: auto;
+    margin-left: auto;
+    margin-bottom: 30px;
+    border: 1px solid brown;
+    align-items: center;
+}
+/* .main{
+    background-color: aqua !important;
+} */
+.register button{
+    width: 320px;
+    height: 40px;
+    background-color: brown;
+    border: 1px solid brown;
+    color: white;
+}
+
+</style>
