bfbf4d80b2ab4e87c12e02ebc0a3701a9fa2224e
Subramanya K S <subramanyaks2205@gmail.com>
2022-12-04 22:39:17 +0530

    added add restuarents

---
+++ src/components/Add.vue
@@ -0,0 +1,21 @@
+<template>
+<Header/>
+<h1>Hello User, Welcome to Add Restuarant Page</h1>
+</template>
+<script>
+import Header from './Header.vue';
+
+export default {
+    name:'Add',
+    components:{
+        Header
+    },
+    mounted(){
+        let users= localStorage.getItem('user-info');
+        if(!users){
+             this.$router.push({name:'SignUp'});
+        }
+    }
+}
+
+</script>
