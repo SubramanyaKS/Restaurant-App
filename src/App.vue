32ea340e71c15934bbf176ac34b47b97b6ecfccd
Subramanya K S <subramanyaks2205@gmail.com>
2022-12-04 13:06:57 +0530

    added app

--- src/App.vue
+++ src/App.vue
@@ -17,4 +17,28 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.logo{
+  width:100px;
+}
+.register input, .login input{
+    width:300px;
+    height:40px;
+    display:block;
+    padding-left: 20px;
+    margin-right: auto;
+    margin-left: auto;
+    margin-bottom: 30px;
+    border: 1px solid brown;
+    align-items: center;
+}
+/* .main{
+    background-color: aqua !important;
+} */
+.register button, .login button{
+    width: 320px;
+    height: 40px;
+    background-color: brown;
+    border: 1px solid brown;
+    color: white;
+}
 </style>
