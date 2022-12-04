6a611fc0116b1753a0a951199a07ed83e70afd1c
Subramanya K S <subramanyaks2205@gmail.com>
2022-12-04 16:51:31 +0530

    updated header

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="nav">
+      <a href="#">Home</a>
+      <a href="#">Add Restaurant</a>
+      <a href="#">Update Restaurant</a>
+      <a href="#">LogOut</a>
+  </div>
+</template>
+<script>
+export default {
+  name: "Header",
+};
+</script>
+<style >
+.nav{
+    background-color: #000;
+    overflow: hidden;
+}
+.nav a{
+    color: #fff;
+    float: left;
+    padding: 16px 14px;
+    text-align: center;
+    font-size: 16px;
+    text-decoration: none;
+    margin-right: 5px;
+}
+.nav a:hover{
+    background-color: brown;
+    
+}
+
+</style>
