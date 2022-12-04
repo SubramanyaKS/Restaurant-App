37eed22e1249c88b0530a3d753bc442ce2c1949c
Subramanya K S <subramanyaks2205@gmail.com>
2022-12-04 16:51:52 +0530

    updated header

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /*margin-top: 60px;*/
+}
+body{
+  padding: 0;
+  margin: 0;
 }
 .logo{
   width:100px;
