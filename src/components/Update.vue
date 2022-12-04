3b6741bab23eca116efa9e6445577013222a3adb
Subramanya K S <subramanyaks2205@gmail.com>
2022-12-04 22:39:52 +0530

    added update

---
+++ src/components/Update.vue
@@ -0,0 +1,21 @@
+<template>
+<Header/>
+<h1>Hello User, Welcome to update Restaurant page</h1>
+</template>
+<script>
+import Header from './Header.vue';
+
+export default {
+    name:'Update',
+    components:{
+        Header
+    },
+    mounted(){
+        let users= localStorage.getItem('user-info');
+        if(!users){
+             this.$router.push({name:'SignUp'});
+        }
+    }
+}
+
+</script>
