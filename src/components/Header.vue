cc5466db64cda8863bea7d628bcec6d8610b1d04
Subramanya K S <subramanyaks2205@gmail.com>
2022-12-04 22:39:32 +0530

    updated header with link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="nav">
-      <a href="#">Home</a>
-      <a href="#">Add Restaurant</a>
-      <a href="#">Update Restaurant</a>
+      <router-link to="/">Home</router-link>
+      <router-link to="/add">Add Restaurant</router-link>
+      <router-link to="/update">Update Restaurant</router-link>
       <a v-on:click="logout" href="#">LogOut</a>
   </div>
 </template>
