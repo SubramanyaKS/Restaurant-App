c5edd5dc0f17fe72779dfe6f9b8986f944fd2c35
Subramanya K S <subramanyaks2205@gmail.com>
2022-12-04 13:07:11 +0530

    added login

---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+<img class="logo" alt="Vue logo" src="../assets/logo1.jpg">
+<h1>Login Page</h1>
+<div class="login">
+        
+        <input type="text" v-model="email" placeholder="Enter the Email:" />
+        <input type="password" v-model="password" placeholder="Enter the Password:" />
+        <button v-on:click="signup" class="btn">Login In</button>
+        <p>Not register?<router-link to="/signup">Sign Up</router-link></p>
+    </div>
+</template>
+
+<script>
+
+export default{
+    name:'LogIn',   
+}
+</script>
