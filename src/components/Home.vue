83da2db084da214df78a793dac6a814c4fd89680
Subramanya K S <subramanyaks2205@gmail.com>
2022-12-04 22:39:40 +0530

    updated home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,26 @@
 <template>
-<Header/>
-<h1> Welcome to home page</h1>
+  <Header />
+  <h1>Hello {{name}}, Welcome to home page</h1>
 </template>
 <script>
-import Header from './Header.vue';
+import Header from "./Header.vue";
 
 export default {
-    name:'HomePage',
-    components:{
-        Header
-    },
-    mounted(){
-        let users= localStorage.getItem('user-info');
-        if(!users){
-             this.$router.push({name:'SignUp'});
-        }
+  name: "Home",
+  data() {
+    return {
+      name: "",
+    };
+  },
+  components: {
+    Header,
+  },
+  mounted() {
+    let users = localStorage.getItem("user-info");
+    this.name= JSON.parse(users).name;
+    if (!users) {
+      this.$router.push({ name: "SignUp" });
     }
-}
-
+  },
+};
 </script>
