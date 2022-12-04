b1fd0954aa7e565896a11db4ee6dbe48006661a2
Subramanya K S <subramanyaks2205@gmail.com>
2022-12-04 16:29:33 +0530

    updated login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,14 +5,38 @@
         
         <input type="text" v-model="email" placeholder="Enter the Email:" />
         <input type="password" v-model="password" placeholder="Enter the Password:" />
-        <button v-on:click="signup" class="btn">Login In</button>
+        <button v-on:click="login" class="btn">Login In</button>
         <p>Not register?<router-link to="/signup">Sign Up</router-link></p>
     </div>
 </template>
 
 <script>
-
+import axios from 'axios';
 export default{
     name:'LogIn',   
+    data(){
+        return{
+            email:'',
+            password:'',
+        }
+    },
+    methods:{
+        async login(){
+            let r = await axios.get(`http://localhost:3000/user?email=${this.email}&& password=${this.password}`)
+            console.warn(r);
+            if(r.status==200 && r.data.length>0){
+              // console.log("Sign-Up done");
+               localStorage.setItem("user-info",JSON.stringify(r.data[0]));
+               this.$router.push({name:'Home'});
+            }
+        }
+
+    },
+    mounted(){
+        let users= localStorage.getItem('user-info');
+        if(users){
+             this.$router.push({name:'Home'});
+        }
+    }
 }
 </script>
