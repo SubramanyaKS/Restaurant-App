e3599351144a09dbef9dd9fa1557bb55e8cad2e0
Subramanya K S <subramanyaks2205@gmail.com>
2022-12-07 07:34:30 +0530

    deleted update navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,6 @@
   <div class="nav">
       <router-link to="/">Home</router-link>
       <router-link to="/add">Add Restaurant</router-link>
-      <router-link to="/update">Update Restaurant</router-link>
       <a v-on:click="logout" href="#">LogOut</a>
   </div>
 </template>
