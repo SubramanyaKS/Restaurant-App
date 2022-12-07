5f9a74d9d201998579547d47ad5e1a737d622d64
Subramanya K S <subramanyaks2205@gmail.com>
2022-12-07 07:34:55 +0530

    updated the update restuarant

--- src/components/Update.vue
+++ src/components/Update.vue
@@ -1,6 +1,12 @@
 <template>
 <Header/>
 <h1>Hello User, Welcome to update Restaurant page</h1>
+<form class="add">
+      <input type="text" name="name" v-model="restuarant.name" placeholder="Enter the Name"/>
+      <input type="text" name="address" v-model="restuarant.address" placeholder="Enter the Address"/>
+      <input type="text" name="contact" v-model="restuarant.contact" placeholder="Enter the Contact"/>
+      <button v-on:click="addRest" type="button">Update Restuarant</button>
+  </form>
 </template>
 <script>
 import Header from './Header.vue';
@@ -10,6 +16,15 @@ export default {
     components:{
         Header
     },
+     data(){
+      return{
+          restuarant:{
+              name:'',
+              address:'',
+              contact:'',
+          }
+      }
+  },
     mounted(){
         let users= localStorage.getItem('user-info');
         if(!users){
